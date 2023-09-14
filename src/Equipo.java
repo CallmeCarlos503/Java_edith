1bd63313fb8d00ea38bba1d6dc38a7370882c6db
CallmeCarlos503 <Carlosgeek503@gmail.com>
2023-09-13 23:39:31 -0600

    Initial commit

---
+++ src/Equipo.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+// Clase base Equipo
+class Equipo {
+    protected String fabricante;
+    protected String modelo;
+    protected String microprocesador;
+
+    public Equipo(String fabricante, String modelo, String microprocesador) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.microprocesador = microprocesador;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getMicroprocesador() {
+        return microprocesador;
+    }
+
+    public String toString() {
+        return "Fabricante: " + fabricante + "\nModelo: " + modelo + "\nMicroprocesador: " + microprocesador;
+    }
+}
